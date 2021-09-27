674e0846f2dfbcb08e2dc05fcb09b7336d235010
Julia Lapenko <yukki.lapenko@gmail.com>
2021-09-27 11:25:53 +0300

    Ready.

--- 03_TerminalProject/Show.c
+++ 03_TerminalProject/Show.c
@@ -2,6 +2,9 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define DX 3
 
 void read_lines(char ***dest_lines, size_t *dest_n, FILE *file) {
   assert(file && "File must be opened!");
@@ -45,7 +48,59 @@ void free_lines(char **lines, size_t n) {
   free(lines);
 }
 
+size_t get_num_len(size_t n) {
+  size_t res = 0;
+  do
+    ++res;
+  while ((n /= 10));
+  return res;
+}
 
+void draw(char **lines, size_t n, const char *filename) {
+  initscr();
+  noecho();
+  cbreak();
+  const char title_file[] = "File: ";
+  const char title_size[] = ", size: ";
+  const size_t n_length = 20;
+  size_t total_max_length = sizeof(title_file) + strlen(filename) +
+                            sizeof(title_file) + n_length + 2;
+  char *title = calloc(total_max_length, sizeof(char));
+  snprintf(title, total_max_length, "%s%s%s%lu", title_file, filename,
+           title_size, n);
+  printw(title);
+  refresh();
+  size_t box_width = COLS - 2*DX, box_height = LINES - 2*DX;
+  WINDOW *win = newwin(box_height, box_width, DX, DX);
+  keypad(stdscr, TRUE);
+  keypad(win, TRUE);
+  curs_set(0);
+  scrollok(win, TRUE);
+  size_t text_height = box_height-2;
+  int di = 0;
+  int c = 0;
+  size_t num_len = get_num_len(n);
+  do {
+    if (c == ' ' || c == KEY_DOWN) {
+      if (di + text_height < n)
+        ++di;
+    } else if (c == KEY_UP) {
+      if (di > 0)
+        --di;
+    }
+    wclear(win);
+    for (size_t i = 0; i < text_height; ++i) {
+      if (i + di > n - 1)
+        break;
+      mvwprintw(win, i + 1, 1, "%*lu: %s\n", num_len + 1, i + di + 1,
+                lines[i + di]);
+      box(win, 0, 0);
+    }
+    wrefresh(win);
+  } while ((c = getch()) != 27);
+  endwin();
+  free(title);
+}
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -61,10 +116,8 @@ int main(int argc, char **argv) {
   size_t n;
   char **lines;
   read_lines(&lines, &n, file);
-  for (size_t i = 0; i < n; ++i) {
-    printf("%s\n", lines[i]);
-  }
-  free_lines(lines, n);
   fclose(file);
+  draw(lines, n, filename);
+  free_lines(lines, n);
   return 0;
 }
