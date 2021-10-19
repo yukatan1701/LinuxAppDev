80a196a3f3a488bac95cf2de60863efb95707b9e
Julia Lapenko <yukki.lapenko@gmail.com>
2021-10-19 16:50:16 +0300

    Final version.

--- 05_Debugging/range.c
+++ 05_Debugging/range.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
   int M = argc > 2 ? atoi(argv[1]) : 0;
   int N = argc > 2 ? atoi(argv[2]) : atoi(argv[1]);
   int S = argc > 3 ? atoi(argv[3]) : 1;
-  for (int I = M; I < N; I += S) {
+  for (int I = M; (S > 0 && I < N) || (S < 0 && I > N); I += S) {
     printf("%d\n", I);
   }
   return 0;
